deaf85e518f63da44ea3282c5275931a9acf9c57
WillKopa <will.kopaczewski@gmail.com>
2025-08-26 13:47:23 -0400

    Updated readme (#1)
    * Updated readme
    
    * Added github actions
    
    * Removed fail and replaced with a command to get the go version
    
    * added tests for auth
    
    * corrected spelling
    
    * Breaking code to ensure CI is setup properly
    
    * Fixed tests after verifying Ci is setup properly
    
    * Added flag to show coverage
    
    * Updated readme with githubs dynamically generated badge.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	token := "frodo"
+	prefix := "ApiKey "
+
+	header1 := http.Header{}
+	header1.Set("Authorization", prefix + token)	
+
+	no_prefix_header := http.Header{}
+	no_prefix_header.Set("Authorization", token)
+
+	tests := []struct{
+		name				string
+		expected_token		string
+		header				http.Header
+		expect_error		bool
+	}{
+		{
+			name: 			"Header with correct syntax",
+			expected_token: token,
+			header:   		header1,
+			expect_error: 	false,
+		},
+		{
+			name: 			"Header with no auth",
+			expected_token: "",
+			header:    		http.Header{},
+			expect_error: 	true,
+		},
+		{
+			name:			"Header without API key",
+			expected_token: "",
+			header:    		no_prefix_header,
+			expect_error: 	true,
+		},
+	}
+
+	for _, token_test := range tests {
+		t.Run(token_test.name, func(t *testing.T) {
+			result, err := GetAPIKey(token_test.header)
+			if (err != nil) && !token_test.expect_error {
+				t.Errorf("error calling GetAPIKey %v", err)
+			}
+			if (result != token_test.expected_token) {
+				t.Errorf("error calling GetAPIKey. Got %s, but expected %s", result, token_test.expected_token)
+			}
+		})
+	}
+}
